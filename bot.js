ac508814000426fae5cf495da70d8426330059f0
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-08-21 00:29:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,29 @@ client.user.setGame(``,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+const adminprefix = "B";
+const devs = ['348530664108195851'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} My game is now **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : The bot name is changed to`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : Done look at my new good avatar :sunglasses:`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**My twitch is now :  ${argresult}**`)
+}
 
+});
 client.login(process.env.BOT_TOKEN);
